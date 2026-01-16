941294945d36d6f38eaf7fda63b762937fa176ec
MhZ9191 <hegic91@gmail.com>
2026-01-16 14:26:18 +0100

    Added snack 1

---
+++ snack1/script.js
@@ -0,0 +1,25 @@
+/*
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+const bicycles = [
+  { name: "MTB", weight: 7.5 },
+  { name: "Elettrica", weight: 8 },
+  { name: "Da corsa", weight: 4 },
+  { name: "Graziella", weight: 10.4 },
+];
+
+//Assegno alla variabile il peso del primo elemento se esiste
+let minWeight;
+if (bicycles[0].weight) minWeight = bicycles[0].weight;
+
+//Ciclo confrontando se il peso del secondo elemento è minore ecc..
+for (let bicycle of bicycles) {
+  const tmpWeight = bicycle.weight;
+  if (tmpWeight < minWeight) minWeight = tmpWeight;
+}
+
+console.log(minWeight);
