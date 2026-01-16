487405a5bad7bc2a2c67c056d2001dd7786a5845
MhZ9191 <hegic91@gmail.com>
2026-01-16 14:43:23 +0100

    Added other for snack 1

--- snack1/script.js
+++ snack1/script.js
@@ -8,21 +8,24 @@ Stampare a schermo la bici con peso minore.
 const bicycles = [
   { name: "MTB", weight: 7.5 },
   { name: "Elettrica", weight: 8 },
-  { name: "Da corsa", weight: 4 },
+  { name: "Da corsa", weight: 3 },
   { name: "Graziella", weight: 10.4 },
 ];
 
 //Assegno alla variabile il peso del primo elemento se esiste
 let minWeight;
 if (bicycles[0].weight) minWeight = bicycles[0].weight;
-
+let outName = "";
 //Ciclo confrontando se il peso del secondo elemento è minore ecc..
 for (let bicycle of bicycles) {
   const tmpWeight = bicycle.weight;
-  if (tmpWeight < minWeight) minWeight = tmpWeight;
+  const tmpName = bicycle.name;
+  if (tmpWeight < minWeight) {
+    minWeight = tmpWeight;
+    outName = tmpName;
+  }
 }
-
-console.log(minWeight);
+console.log(`La bici con peso minore è la bici ${outName} con ${minWeight}Kg`);
 
 //! Versione con map()
 
@@ -34,3 +37,11 @@ const tmpArray = bicycles.map((el) => {
   if (elWeight < mini) mini = elWeight;
 });
 console.log(mini);
+
+//! Versione compatta
+
+let minimo;
+if (bicycles[0].weight) minimo = bicycles[0].weight;
+
+bicycles.map((el) => (el.weight < minimo ? (minimo = el.weight) : null));
+console.log(minimo);
